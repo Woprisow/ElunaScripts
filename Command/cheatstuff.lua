e0e67b28828761019737491439a01b9ce0663959
RainDash <43726364+DevRainDash@users.noreply.github.com>
2018-10-26 13:23:06 +0200

    Cheatstuff or GmStuff

---
+++ Command/cheatstuff.lua
@@ -0,0 +1,15 @@
+local function OnPlayerCommand(event, player, command)
+    gmRank = player:GetGMRank()
+    if (gmRank > 0 and command == "cheatstuff")then
+        player:AddItem(43651)
+        player:AddItem(12064)
+        player:AddItem(2586)
+        player:AddItem(11508)
+        player:AddItem(18582)
+        player:AddItem(17)
+        player:AddItem(192)
+        return false
+    end
+end
+
+RegisterPlayerEvent(42, OnPlayerCommand)
