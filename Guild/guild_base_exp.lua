c50dd55213f874b955a7937bab2d694296f9adbd
RainDash <43726364+DevRainDash@users.noreply.github.com>
2018-10-26 21:46:57 +0200

    U.A.C
    U . A = If the guild goes up one level, everyone knows, thanks to a global announcement.
    C = The calculation of xp is now fixed, if you have the max number of exp required, your guild will now pass a level.

--- Guild/guild_base_exp.lua
+++ Guild/guild_base_exp.lua
@@ -16,21 +16,28 @@ local function OnGuildMemberKillCreature (event, player, creature)
       Calcul.ClassicExp = ((GetFunction.PlayerLevel * 5) + 45)
       Calcul.BcExp = ((GetFunction.PlayerLevel * 5) + 235)
       Calcul.NorthendExp = ((GetFunction.PlayerLevel * 5) + 580)
+
+    Message = {}
+      Message.Congratulation = "Congratulations your guild just went up one level now it's level "
 -- CLASSIC EXP --
     for MapId,MapName in pairs(ClassicMapID) do
       if(MapId == GetFunction.PlayerMap)then
-        local Q0 = CharDBQuery("SELECT final_exp, level, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+        local Q0 = CharDBQuery("SELECT level FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
         if(Q0 ~= nil)then
-          local final_exp = Q0:GetUInt32(0)
-          local level = Q0:GetUInt32(1)
-          local current_exp = Q0:GetUInt32(2)
+          local level = Q0:GetUInt32(0)
           if(Calcul.HighLevel <= 5 or Calcul.LowLevel >= -5 and level ~= 24)then
             local Q1 = CharDBQuery("UPDATE `eluna`.`guild_members_stats` SET totalexp = totalexp + "..Calcul.ClassicExp.." WHERE member_id = "..GetFunction.PlayerGUID.."")
             local Q2 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = current_exp + "..Calcul.ClassicExp.." WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            local Q0 = CharDBQuery("SELECT final_exp, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            --
+            local final_exp = Q0:GetUInt32(0)
+            local current_exp = Q0:GetUInt32(1)
             if(current_exp >= final_exp)then
               local Q3 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = 0 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
               local Q4 = CharDBQuery("UPDATE `eluna`.`guild_base` SET final_exp = final_exp * 2 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
               local Q5 = CharDBQuery("UPDATE `eluna`.`guild_base` SET level = level + 1 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+              local levelup = level +1
+              SendWorldMessage("|CFF00A728Congratulations to the guild "..player:GetGuildName().." which has just passed level "..levelup.."!")
             end
           end
         end
@@ -39,14 +46,16 @@ local function OnGuildMemberKillCreature (event, player, creature)
 -- BC EXP --
     for MapId,MapName in pairs(BCMapID) do
       if(MapId == GetFunction.PlayerMap)then
-        local Q0 = CharDBQuery("SELECT final_exp, level, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+        local Q0 = CharDBQuery("SELECT level FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
         if(Q0 ~= nil)then
-          local final_exp = Q0:GetUInt32(0)
-          local level = Q0:GetUInt32(1)
-          local current_exp = Q0:GetUInt32(2)
+          local level = Q0:GetUInt32(0)
           if(Calcul.HighLevel <= 5 or Calcul.LowLevel >= -5 and level ~= 24)then
             local Q1 = CharDBQuery("UPDATE `eluna`.`guild_members_stats` SET totalexp = totalexp + "..Calcul.BcExp.." WHERE member_id = "..GetFunction.PlayerGUID.."")
             local Q2 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = current_exp + "..Calcul.BcExp.." WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            local Q0 = CharDBQuery("SELECT final_exp, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            --
+            local final_exp = Q0:GetUInt32(0)
+            local current_exp = Q0:GetUInt32(1)
             if(current_exp >= final_exp)then
               local Q3 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = 0 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
               local Q4 = CharDBQuery("UPDATE `eluna`.`guild_base` SET final_exp = final_exp * 2 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
@@ -59,14 +68,16 @@ local function OnGuildMemberKillCreature (event, player, creature)
 -- TLK EXP --
     for MapId,MapName in pairs(TLKMapID) do
       if(MapId == GetFunction.PlayerMap)then
-        local Q0 = CharDBQuery("SELECT final_exp, level, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+        local Q0 = CharDBQuery("SELECT level FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
         if(Q0 ~= nil)then
-          local final_exp = Q0:GetUInt32(0)
-          local level = Q0:GetUInt32(1)
-          local current_exp = Q0:GetUInt32(2)
+          local level = Q0:GetUInt32(0)
           if(Calcul.HighLevel <= 5 or Calcul.LowLevel >= -5 and level ~= 24)then
             local Q1 = CharDBQuery("UPDATE `eluna`.`guild_members_stats` SET totalexp = totalexp + "..Calcul.BcExp.." WHERE member_id = "..GetFunction.PlayerGUID.."")
             local Q2 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = current_exp + "..Calcul.BcExp.." WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            local Q0 = CharDBQuery("SELECT final_exp, current_exp FROM `eluna`.`guild_base` WHERE guild_id = "..GetFunction.PlayerGuildId.."")
+            --
+            local final_exp = Q0:GetUInt32(0)
+            local current_exp = Q0:GetUInt32(1)
             if(current_exp >= final_exp)then
               local Q3 = CharDBQuery("UPDATE `eluna`.`guild_base` SET current_exp = 0 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
               local Q4 = CharDBQuery("UPDATE `eluna`.`guild_base` SET final_exp = final_exp * 2 WHERE guild_id = "..GetFunction.PlayerGuildId.."")
